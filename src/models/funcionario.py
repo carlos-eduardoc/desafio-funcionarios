1854eb0d1227614481586f2cfb922b2496409bdc
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-14 09:51:01 -0300

    feat: Classe Funcionario abstrata, dentro dela adicionei os atributos nome e salario que é protegido inicialmente, utilizei encapsulamento(property) para validar entrada e saida, criei um metodo abstrato para calcular bonus, tambem usei operator overloading no dunder method str

---
+++ src/models/funcionario.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+class Funcionario(ABC):
+    def __init__(self, nome, salario):
+        self.nome = nome
+        self._salario = salario
+    
+    def __str__(self):
+        return f'O {self.__class__.__name__} tem o salario de {self.salario} e por ser {self.__class__.__name__} tem o bonus de {self.calcular_bonus()}'
+    
+    
+    @property
+    def salario(self):
+        return f'R${self._salario:,.2f}'
+    
+    @salario.setter
+    def salario(self, valor):
+        if valor < self._salario:
+            raise ValueError('Você não pode reduzir o salario do funcionario')
+        self.__salario = valor
+    
+    
+    @abstractmethod
+    def calcular_bonus(self):
+        pass
