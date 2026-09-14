82a2b2c7d307c2791f3bf4bef741bc5b8e9b04b6
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-14 09:53:41 -0300

    feat: Classe Desenvolvedor, herdou de Funcionario, reescrevendo o metodo de calcular bonus de 10%

---
+++ src/models/dev.py
@@ -0,0 +1,16 @@
+from models.funcionario import Funcionario
+
+class Desenvolvedor(Funcionario):
+    def __init__(self, nome, salario):
+        super().__init__(nome, salario)
+    
+    
+    def calcular_bonus(self):
+        taxa = 10
+        bonus = (self._salario / 100) * taxa
+    
+        return f'R${bonus:,.2f}'
+
+    
+    def __str__(self):
+        return super().__str__()
